80c01998df051ae1c9de37331e86f1e8645dbee6
DELL <DELL@Keysi-PC>
2023-10-08 00:20:50 -0400

    Adiccion de paquetes y conexion

--- SaludCura/src/java/config/conexion.java
+++ SaludCura/src/java/config/conexion.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package config;
 
 /**
  *
  * @author DELL
  */
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class conexion {
+   
     
+    public conexion getConnection(){
+        try {
+    String myBD = /*aca la URL de la BD */ "jdbc:mysql://localhost:3306/rentcar?serverTimezone=UTC";
+            java.sql.Connection con = DriverManager.getConnection(myBD,"root", "" );
+    return (conexion) con;
+    
+    } catch(SQLException e){
+        
+        System.out.println(e.toString());
+        return null;
+    }
+  }  
+
+    public PreparedStatement prepareStatement(String select_idvehicle_Caption_VIN_MotorNo_VLP_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
+
+
